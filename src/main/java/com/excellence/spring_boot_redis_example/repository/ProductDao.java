df41f0869c7a9012d25be30009869abef0c829fc
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-28 17:17:46 +0530

    Added redis database, cache and readme file

---
+++ src/main/java/com/excellence/spring_boot_redis_example/repository/ProductDao.java
@@ -0,0 +1,36 @@
+package com.excellence.spring_boot_redis_example.repository;
+
+import com.excellence.spring_boot_redis_example.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductDao {
+
+    public static final String HASH_KEY = "PRODUCT";
+
+    @Autowired
+    private RedisTemplate template;
+
+    public Product save(Product product) {
+        template.opsForHash().put(HASH_KEY, product.getId(), product);
+        return product;
+    }
+
+    public List<Product> findAll() {
+        return template.opsForHash().values(HASH_KEY);
+    }
+
+    public Product findProductById(int id) {
+        System.out.println("called findProductById() from DB");
+        return (Product) template.opsForHash().get(HASH_KEY, id);
+    }
+
+    public String deleteProduct(int id) {
+        template.opsForHash().delete(HASH_KEY, id);
+        return "product removed !!";
+    }
+}
