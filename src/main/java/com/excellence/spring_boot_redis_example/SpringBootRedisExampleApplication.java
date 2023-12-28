9a4158a599fe37c262fe097e7e4ff2a5f771dc4b
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-28 13:05:29 +0530

    initialize the project

---
+++ src/main/java/com/excellence/spring_boot_redis_example/SpringBootRedisExampleApplication.java
@@ -0,0 +1,13 @@
+package com.excellence.spring_boot_redis_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/excellence/spring_boot_redis_example/SpringBootRedisExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.spring_boot_redis_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
